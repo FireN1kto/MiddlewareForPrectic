e0146e25b94b23ba210d24966cf42c2c9b5b7878
F1reN1kto <maksimcymbal77@gmail.com>
2025-10-08 12:07:41 +0700

    End of work on an individual assignment

--- src/AuthMiddleware.php
+++ src/AuthMiddleware.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace Middlewares;
+
+use Src\Auth\Auth;
+use Src\Request;
+
+class AuthMiddleware
+{
+    public function handle(Request $request)
+    {
+        if (!Auth::check()) {
+            app()->route->redirect('/login');
+            return false;
+        }
+        return true;
+    }
+}
--- src/OfficerMiddleware.php
+++ src/OfficerMiddleware.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace Middlewares;
+
+use Model\User;
+use Src\Auth\Auth;
+use Src\Request;
+
+class OfficerMiddleware
+{
+    public function handle(Request $request)
+    {
+        $user = Auth::user();
+        if (!$user || $user->role->name_role !== 'registration_officer') {
+            app()->route->redirect('/hello');
+            return false;
+        }
+        error_log('OfficerMiddleware executed for user: ' . Auth::user()->id);
+        return true;
+    }
+}
